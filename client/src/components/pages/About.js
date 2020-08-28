e49b05c97f247bd34be5139a521844058cc23cba
Ashkan Gholizadeh <ashkan.ghk84@gmail.com>
2020-08-28 17:45:38 +0100

    client side set up-pages

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p className="my-1">Full Stack MERN app</p>
+        </div>
+    )
+}
+
+export default About;
